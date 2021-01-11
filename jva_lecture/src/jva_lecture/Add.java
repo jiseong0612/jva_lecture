01f9c2724ae9a91baf55f550138b9696961a3f98
hth55 <hth55@LAPTOP-BALEGLFD>
2021-01-11 13:49:59 +0900

    Add

---
+++ jva_lecture/src/jva_lecture/Add.java
@@ -0,0 +1,13 @@
+package jva_lecture;
+
+public class Add {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		int x=10;
+		int y=10;
+		int result = x+y;
+		System.out.println(result);
+	}
+
+}
